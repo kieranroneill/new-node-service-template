fa6508a861575c79d920e3fa85a69c455057e0d3
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-12-10 23:09:19 +0100

    feat: adds more information to healthcheck route

--- src/api/healthcheck/controller.ts
+++ src/api/healthcheck/controller.ts
@@ -5,6 +5,10 @@ import BaseController from '../base/controller';
 
 export default class Controller extends BaseController {
   public get(req: Request, res: Response): void {
-    res.status(200).send('OK').end();
+    res.status(200).json({
+      environment: process.env.NODE_ENV,
+      name: process.env.SERVICE_NAME,
+      version: process.env.VERSION,
+    });
   }
 }
